1d356e6307e24a7ac96ab6b1433d4a62a03f5d27
JimmyX304 <hxu226@gmail.com>
2026-05-19 21:21:33 -0700

    done with checkpoint

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -5,20 +5,25 @@ public class ArrayDeque<T> {
     private int size;
 
     public ArrayDeque() {
-        items = (T[]) new Object[100];
+        items = (T[]) new Object[8];
         size = 0;
     }
 
     public void addFirst(T item) {
-
+        for (int i = size; i >= 1; i--) {
+            items[i] = items[i - 1];
+        }
+        items[0] = item;
+        size++;
     }
 
     public void addLast(T item) {
-
+        items[size] = item;
+        size++;
     }
 
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     public int size() {
@@ -26,18 +31,30 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + " ");
+        }
+        System.out.println();
     }
 
     public T removeFirst() {
-
+        T res = items[0];
+        for (int i = 0; i < size - 1; i++) {
+            items[i] = items[i + 1];
+        }
+        items[size - 1] = null;
+        size--;
+        return res;
     }
 
     public T removeLast() {
-
+        T res = items[size - 1];
+        items[size - 1] = null;
+        size--;
+        return res;
     }
 
     public T get(int index) {
-
+        return items[index];
     }
 }
