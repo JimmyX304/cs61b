8117df424c0c8ac9da2a8aea0a47a76fe23d519e
JimmyX304 <hxu226@gmail.com>
2026-05-21 15:38:02 -0700

    added resizing to ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -9,7 +9,16 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
     public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
         for (int i = size; i >= 1; i--) {
             items[i] = items[i - 1];
         }
@@ -18,6 +27,9 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
         items[size] = item;
         size++;
     }
@@ -41,7 +53,11 @@ public class ArrayDeque<T> {
         if (size == 0) {
             return null;
         }
-        
+
+        if (size < items.length / 4 && items.length >= 32) {
+            resize(items.length / 4);
+        }
+
         T res = items[0];
         for (int i = 0; i < size - 1; i++) {
             items[i] = items[i + 1];
@@ -54,6 +70,10 @@ public class ArrayDeque<T> {
     public T removeLast() {
         if (size == 0) {
             return null;
+        }
+
+        if (size < items.length / 4 && items.length >= 32) {
+            resize(items.length / 4);
         }
 
         T res = items[size - 1];
