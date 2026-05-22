5785eeafde9bdec8b577bd2fda014a51196182ed
JimmyX304 <hxu226@gmail.com>
2026-05-22 11:19:44 -0700

    fixed MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,29 +3,28 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    Comparator<T> prevComparator;
-    ArrayDeque<T> ad = new ArrayDeque<>();
+    private Comparator<T> defaultComparator;
+    private ArrayDeque<T> ad = new ArrayDeque<>();
 
     public MaxArrayDeque(Comparator<T> c) {
-        prevComparator = c;
+        defaultComparator = c;
     }
 
     public T max() {
+        return max(defaultComparator);
+    }
+
+    public T max(Comparator<T> c) {
         if (ad.isEmpty()) {
             return null;
         }
         T best = ad.get(0);
         for (int i = 0; i < ad.size(); i++) {
-            int cmp = prevComparator.compare(ad.get(i), best);
+            int cmp = c.compare(ad.get(i), best);
             if (cmp > 0) {
                 best = ad.get(i);
             }
         }
         return best;
-    }
-
-    public T max(Comparator<T> c) {
-        prevComparator = c;
-        return max();
     }
 }
