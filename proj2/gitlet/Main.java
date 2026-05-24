0a3aaf8522250e3940e28aaac5da71b9bdd09ef3
JimmyX304 <hxu226@gmail.com>
2026-05-24 13:35:21 -0700

    made cases in Main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,16 +9,135 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        int length = args.length;
+        if (length == 0) {
+            System.out.println("Please enter a command.");
+            return;
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
                 // TODO: handle the `init` command
+
+                if (length != 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+                
+
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+
+                if (length == 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+                break;
+            case "rm":
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+                break;
+            case "log":
+
+                if (length != 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+                break;
+            case "global-log":
+
+                if (length != 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+                break;
+            case "find":
+
+                if (length == 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            case "status":
+
+                if (length != 1) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            case "checkout":
+
+
+                break;
+            case "branch":
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            case "rm-branch":
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            case "reset":
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            case "merge":
+
+                if (length != 2) {
+                    System.out.println("Incorrect operands.");
+                    break;
+                }
+
+
+
+                break;
+            default:
+                System.out.println("No command with that name exists.");
         }
     }
 }
